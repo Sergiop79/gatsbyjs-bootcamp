a91b332640a6748871eab2309537cc61a4a0c6b9
Sergio Peña <sergiop79@gmail.com>
2019-07-19 19:12:01 +0200

    Lesson #20 Deploying Your Gatsby Site

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,6 @@ const IndexPage = () => {
       <h2>
         I'm Andrew, a full-stack developer living in beautiful Philadelphia.
       </h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
